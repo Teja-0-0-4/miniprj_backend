1d95717bdca148a49f94e38f48543955df299286
Kukatla Teja <tejaspix1@gmail.com>
2024-06-14 22:07:42 +0530

    updated url and products

--- url.js
+++ url.js
@@ -1,3 +1,3 @@
 // url.js
 // module.exports = 'mongodb://localhost:27017/'
-module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/"
+module.exports ="mongodb+srv://admin:admin@cluster0.viv4vl9.mongodb.net/"
